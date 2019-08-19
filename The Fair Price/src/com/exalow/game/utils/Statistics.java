28d3825d8a68965510f793d4ae76fd12d9508869
Exalow <53000164+Exalow@users.noreply.github.com>
2019-08-19 18:37:37 +0200

    Add files via upload

---
+++ The Fair Price/src/com/exalow/game/utils/Statistics.java
@@ -0,0 +1,21 @@
+package com.exalow.game.utils;
+
+import com.exalow.game.TheFairPrice;
+
+public class Statistics {
+
+    private long time = TheFairPrice.getTime();
+    private int tries = TheFairPrice.getTries();
+
+    public final void getStatistics() {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n----------[STATS]----------");
+        stringBuilder.append("\n         Time : " + time +  "s");
+        stringBuilder.append("\n         Tries : " + tries);
+        stringBuilder.append("\n---------------------------");
+
+        System.out.println(stringBuilder);
+    }
+
+}
