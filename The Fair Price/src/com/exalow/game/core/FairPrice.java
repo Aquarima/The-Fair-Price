33e2996b660fea96f0ea80290d2a5bb10e844853
Exalow <53000164+Exalow@users.noreply.github.com>
2019-08-20 19:05:47 +0200

    Add files via upload

--- The Fair Price/src/com/exalow/game/core/FairPrice.java
+++ The Fair Price/src/com/exalow/game/core/FairPrice.java
@@ -2,15 +2,62 @@ package com.exalow.game.core;
 
 import com.exalow.game.utils.Difficulty;
 
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class FairPrice {
 
-    private Difficulty d = new Difficulty();
-    private int maxPrice = new Difficulty().getMaxPrice();
+    private Difficulty d;
+    private String difficulty = Difficulty.getSelectedDifficulty();
+    private int maxPrice;
+    private int price;
+    private static int tries;
+    private static long start, end;
 
     public FairPrice() {
 
-        System.out.println(maxPrice);
+        int input = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        maxPrice = Difficulty.Difficuties.valueOf(difficulty).getMaxPrice();
+        price = random.nextInt(maxPrice);
+
+        System.out.println("\nChoose a number between 0 <-> " + maxPrice);
+
+        start = System.currentTimeMillis();
+        while (input != price) {
+
+            try {
+                input = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                throw new RuntimeException("Invalid value !");
+            }
+
+            if (input > maxPrice) {
+                System.err.println("Price cannot be greater than " + maxPrice + " !");
+            } else if (input < price) {
+                System.out.println("More !");
+            } else {
+                System.out.println("Less !");
+            }
+
+            tries++;
+
+        }
+        end = System.currentTimeMillis();
+
+        System.out.println("\nCongratulations, The fair price was " + price + " !");
 
     }
 
+    public static long getTime() {
+        return (end - start) / 1000;
+    }
+
+    public static int getTries() {
+        return tries;
+    }
 }
