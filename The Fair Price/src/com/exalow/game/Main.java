4eafef1a1c75b498535bd79c2ae92294eb8eadcf
Exalow <53000164+Exalow@users.noreply.github.com>
2019-08-20 13:07:18 +0200

    Add files via upload

--- The Fair Price/src/com/exalow/game/Main.java
+++ The Fair Price/src/com/exalow/game/Main.java
@@ -1,11 +1,15 @@
 package com.exalow.game;
 
+import com.exalow.game.core.FairPrice;
 import com.exalow.game.utils.Difficulty;
 
 public class Main {
 
     public static void main(String[] args) {
-        new Difficulty().setDifficulty();
+
+        new Difficulty().Difficulty();
+        new FairPrice();
+
     }
 
 }
